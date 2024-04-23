bba486a75f305887cd1fd716a16449872f076add
Arjun Arasu <arjunbalan1994@gmail.com>
2024-04-23 16:28:33 +0530

    navbar styles changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ function BrandNavbar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Link className='nav-link text-light' to="home" spy={true} smooth={true} duration={500}>Home</Link>
-          <Link className='nav-link text-light' to="products" spy={true} smooth={true} duration={500}>Products</Link>
-          <Link className='nav-link text-light' to="aboutus" spy={true} smooth={true} duration={500}>About Us</Link>
+          <Link className='nav-link text-light btn link-info' to="home" spy={true} smooth={true} duration={500}>Home</Link>
+          <Link className='nav-link text-light btn link-info ' to="products" spy={true} smooth={true} duration={500}>Products</Link>
+          <Link className='nav-link text-light btn link-info' to="aboutus" spy={true} smooth={true} duration={500}>About Us</Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
